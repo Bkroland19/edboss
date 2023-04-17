eb9ebae37be606a48e7c22a89ad6eac1fd3a3052
Bkroland19 <bkroland19@gmail.com>
2023-04-17 16:52:49 +0300

    navbar and banner finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"navbar-background":
+					"url('https://res.cloudinary.com/itgenius/image/upload/v1681737725/dtac-thailand-business-phone-blog_gg79dk.jpg')",
+			},
+		},
+		// colors: {
+		// 	"edboss-orange": "#fa8f3d",
+		// },
+	},
+	plugins: [],
+};
